179f79fe35bcb399751119c4901aae302e8fb852
Volodymyr Kachmar <37980324+volodiakachmar@users.noreply.github.com>
2024-04-28 23:18:53 +0200

    Update store.js

--- client/app/store.js
+++ client/app/store.js
@@ -7,7 +7,7 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { routerMiddleware } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from 'history'; // функция из библиотеки history для создания объекта истории браузера.
 
 import createReducer from './reducers';
 
@@ -21,6 +21,8 @@ const middlewares = [thunk, routerMiddleware(history)];
 const enhancers = [applyMiddleware(...middlewares)];
 
 // If Redux DevTools Extension is installed use it, otherwise use Redux compose
+// Эта часть кода проверяет, установлено ли расширение Redux DevTools в браузере, и использует его, если 
+// оно установлено. В противном случае используется обычный compose.
 const composeEnhancers =
   process.env.NODE_ENV !== 'production' &&
   typeof window === 'object' &&
@@ -33,6 +35,9 @@ const store = createStore(
   composeEnhancers(...enhancers)
 );
 
+// Поддержка горячей перезагрузки (Hot Module Replacement):
+// Этот блок кода позволяет перезагружать корневой редюсер Redux при изменениях, чтобы поддерживать 
+// горячую перезагрузку модулей с помощью Webpack Hot Module Replacement.
 if (module.hot) {
   // Enable Webpack hot module replacement for reducers
   module.hot.accept('./reducers', () => {
